9b2bd78634a9279b9b167affbf47ac27559f2411
Ibrux11 <ibraluunge@gmail.com>
2024-11-17 14:21:54 +0100

    add

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,15 @@
+const cloneSymbol = Symbol('clone');
+class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+  [cloneSymbol]() {
+    return new (Object.getPrototypeOf(this).constructor)(this._brand, this._motor, this._color);
+  }
+  cloneCar() {
+    return this[cloneSymbol]();
+  }
+}
+export default Car;
