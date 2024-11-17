786e701d2a6ea68cde7e6773891c0b969e717ded
Ibrux11 <ibraluunge@gmail.com>
2024-11-17 13:58:48 +0100

    add

--- ES6_basic/100-createIteratorObject.js
+++ ES6_basic/100-createIteratorObject.js
@@ -1,11 +1,16 @@
 export default function createIteratorObject(report) {
-    return {
-      * [Symbol.iterator]() {
-        for (const department of Object.values(report.allEmployees)) {
-          for (const employee of department) {
-            yield employee;
-          }
+  return {
+    * [Symbol.iterator]() {
+
+      const departments = Object.values(report.allEmployees);
+
+
+      for (const department of departments) {
+
+        for (const employee of department) {
+          yield employee;
         }
-      },
-    };
-  }
+      }
+    },
+  };
+}
